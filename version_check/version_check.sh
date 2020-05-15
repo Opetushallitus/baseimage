4331f47c00dd9bb9f24e4f47aa33e4e83bfbba84
Juhani Hietikko <juhani.hietikko@reaktor.com>
2020-05-15 10:14:22 +0300

    Move version check code in here from ci-tools

---
+++ version_check/version_check.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+echo "version check says moi!"
+echo "${ECR_REPO}/${ARTIFACT_NAME}:ci-${TRAVIS_BUILD_NUMBER}"
+
+#if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]
+#then
+#docker run -v ${TRAVIS_BUILD_DIR}:/repository ${DOCKER_TARGET} /bin/sh -c "apk info -v | sort > /repository/package-versions && chmod 755 /repository/package-versions"
+#git diff ${TRAVIS_BUILD_DIR}/package-versions
+#git checkout ${TRAVIS_BRANCH}
+#sudo apt-get update
+#sudo apt-get install -y python3 python3-pip python3-setuptools
+#sudo pip3 install -r $(dirname $0)/requirements.txt
+#python3 $(dirname $0)/version_check.py
+#else
+#  echo "Version check skipped (non scheduled build)"
+#fi
