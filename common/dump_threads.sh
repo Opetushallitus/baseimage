e71331eb42795dba380eb373e03b5faa77cf1d4c
Juhani Hietikko <juhani.hietikko@reaktor.com>
2021-07-28 09:50:48 +0300

    Make dump_threads script compatible with running it as root
    With ECS Exec we have root user and need to change to oph user
    for dumping threads.

--- common/dump_threads.sh
+++ common/dump_threads.sh
@@ -1,2 +1,8 @@
-#!/usr/bin/env sh
-jcmd `pidof java` Thread.print
+#!/usr/bin/env bash
+
+if [[ $(whoami) == "oph" ]]; then
+  /opt/java/openjdk/bin/jcmd "$(pidof java)" Thread.print
+else
+  # Run the command as oph, the java process is also running as oph and this needs to be the same user
+  su - oph bash -c "/opt/java/openjdk/bin/jcmd $(pidof java) Thread.print"
+fi
