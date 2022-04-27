7b57daa36aae425068e7117af5d3eebffc4b2161
Juhani Hietikko <juhani.hietikko@reaktor.com>
2022-04-27 08:39:22 +0300

    Fix git diff command in version check

--- version_check/version_check.sh
+++ version_check/version_check.sh
@@ -3,11 +3,11 @@ set -eo pipefail
 
 for variant in "fatjar-openjdk8" "fatjar-openjdk11" "fatjar-openjdk17" "war-openjdk8" "war-openjdk11" "war-tomcat8-openjdk8" "war-tomcat8-openjdk11"; do
   docker run -u root -v $(pwd)/variants/${variant}:/repository "${ECR_REPO}/baseimage-${variant}:${IMAGE_LABEL}" /bin/sh -c "apk info -v | sort > /repository/package-versions && chmod 755 /repository/package-versions"
+  git diff $(pwd)/variants/${variant}/package-versions
 done
 
 git config --global user.name "Github Actions"
 git config --global user.email "github-actions@opintopolku.fi"
-git diff $(pwd)/package-versions
 git commit -a -m 'Update alpine packages'
 git push origin "HEAD:${GITHUB_REF_NAME}"
 curl -H "Content-Type: application/json" -X POST \
